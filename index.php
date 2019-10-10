40cf8fdc04048ff1bc0a2711f6e287ea6cc1de3f
cefiamidin <cefiamidin@mahasiswa.unikom.ac.id>
2019-10-10 14:46:45 +0700

    pembuatan index dan penambahan link master

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+  <p> Nim : 10116187 <br>
+      Nama : Reza Syahfrizal </p>
+
+      <a href="https://tugas1-master-cc4.herokuapp.com" target="_blank"> Kembali ke Master </a>
+
+</body>
+</html>

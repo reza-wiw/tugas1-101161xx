4f0dba5a576d5047bbd954a7da70d62d5afa969d
sitijuniyanti <sitijuniyanti@gmail.com>
2019-10-12 15:03:08 +0700

    perubahan index dan penambahan vant.js

--- index.php
+++ index.php
@@ -1,9 +1,82 @@
+<!doctype html>
 <html>
-<body>
-  <p> Nim : 10116187 <br>
-      Nama : Reza Syahfrizal </p>
+  <head>
+    <title>TUGAS 1 101161xx-03</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/custom-bs.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="fonts/line-icons/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body id="top">
 
-      <a href="https://tugas1-master-cc4.herokuapp.com" target="_blank"> Kembali ke Master </a>
 
-</body>
+      <!-- <div id="ininet"></div> -->
+
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+
+    <!-- NAVBAR -->
+    <header class="site-navbar mt-3">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="site-logo col-6"><a href="index.html">ANGGOTA</a></div>
+          <div class="right-cta-menu text-right d-flex aligin-items-center col-10">
+            <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+    <!-- HOME -->
+    <section class="home-section section-hero overlay bg-image" id="inirings">
+
+
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-12">
+            <div class="mb-5 text-center">
+              <h1 class="text-white font-weight-bold">ANGGOTA - MASTER</h1>
+
+            </div>
+			<center>
+        <table>
+          <tr>
+        <td><h4 class="text-white"> Reza Syahfrizal </h4></td>
+        <td><h4 class="text-white"> | 10116187  | </h4></td>
+      </tr>
+  </table>
+  <a href="http://tugas1-master-cc4.herokuapp.com/" h4 class="text-white"> Kembali ke master  </a></h4>
+              </div>
+			  </center>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="js/three.r92.min.js"></script>
+    <script src="js/vanta.rings.min.js"></script>
+    <script>
+        VANTA.RINGS({
+            el: "#inirings"
+        })
+    </script>
+  </body>
 </html>
